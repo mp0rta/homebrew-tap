306622171be95151039c2783531e624a0a352e08
mp0rta <3p0rta26@gmail.com>
2026-08-01 22:51:33 +0900

    mqvpn 0.15.0

--- Formula/mqvpn.rb
+++ Formula/mqvpn.rb
@@ -1,9 +1,9 @@
 class Mqvpn < Formula
   desc "Multipath VPN using MASQUE CONNECT-IP (RFC 9484) and Multipath QUIC"
   homepage "https://github.com/mp0rta/mqvpn"
-  version "0.14.1"
+  version "0.15.0"
   url "https://github.com/mp0rta/mqvpn/releases/download/v#{version}/mqvpn_#{version}_darwin_arm64.tar.gz"
-  sha256 "030512073450306991bb5257016fe747c62777735a5f65b907e3f4c77f762830"
+  sha256 "eee9927102c56f7d52d757078c9caee21e048fea68ea0cea3cc329aced28cbaf"
   license "Apache-2.0"
 
   depends_on :macos
